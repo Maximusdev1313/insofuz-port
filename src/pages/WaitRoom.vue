93b5de0c1a50fe9b41e6ae1e0f1194c94fba600d
maximusdev1313 <maximusdev1313@gmail.com>
2023-03-15 00:48:00 +0500

    add pusher-js and get user's informations

--- src/pages/WaitRoom.vue
+++ src/pages/WaitRoom.vue
@@ -1,23 +1,40 @@
 <script setup>
 import {useApiStore} from 'src/stores'
 import { onMounted,ref } from 'vue'
+import axios from 'axios'
+import Pusher from 'pusher-js'
+Pusher.logToConsole = true;
+
+    var pusher = new Pusher('1050f4fd4d96cb76281e', {
+      cluster: 'ap2'
+    });
+
+    var channel = pusher.subscribe('my-channel');
+    channel.bind('my-event', function(data) {
+      app.userInfo.push(JSON.stringify(data));
+    });
 
 const store = useApiStore()
+let userInfo = ref([])
 let status = ref('')
-let userId = store.userId
-let getUserData = async()=>{
-      try{
-        let Response = await axios.get(`http://insofuzlast.pythonanywhere.com/user/${userId}`)
-        let user = Response.data
-        // status.value = user
-        console.log(user);
-    }catch(error){
-        console.log(error.massage);
+let userId = localStorage.getItem('userId')
+let getUserData =  async (id)=>{
+      try {
+        let apiData = await axios.get(`http://insofuzlast.pythonanywhere.com/user/${id}/`)
+        let simpleCategory = apiData.data
+
+        console.log(simpleCategory,'cat');
+        // userInfo.value = [...simpleCategory]
+        // console.log(userInfo.value,'user');
+        
+      } catch (error) {
+          // location.reload()
+          console.log(error);
       }
     }
 onMounted(()=>{
     // setInterval(() => {
-    //  getUserData()
+     getUserData(userId)
         
     // }, 5000);
 })
@@ -25,5 +42,6 @@ onMounted(()=>{
 <template>
     <div>
         user
+        <button @click="getUserData(userId)">sdasd</button>
     </div>
 </template>
