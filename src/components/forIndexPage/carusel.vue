3b1d51f7c6b45395698ce6dfb5d484f2345eefbf
maximusdev1313 <maximusdev1313@gmail.com>
2023-03-19 21:11:38 +0500

    create carusel for index page

---
+++ src/components/forIndexPage/carusel.vue
@@ -0,0 +1,48 @@
+<script setup>
+import {ref} from 'vue'
+let slide = ref('first')
+let autoplay = ref(true)
+</script>
+
+<template>
+  <div class="">
+    <q-carousel
+      arrows
+      animated
+      infinite
+      :autoplay="autoplay"
+      transition-prev="slide-right"
+      transition-next="slide-left"
+      @mouseenter="autoplay = false"
+      @mouseleave="autoplay = true"
+      v-model="slide"
+      height="500px"
+    >
+      <q-carousel-slide name="first" img-src="src/assets/commerce.png">
+        <div class="absolute-bottom custom-caption">
+          <div class="text-h2">First stop</div>
+          <div class="text-subtitle1">Mountains</div>
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="second" img-src="https://www.theasset.com/storage/Image/2021/May/161996875318980.jpg">
+        <div class="absolute-bottom custom-caption">
+          <div class="text-h2">Second stop</div>
+          <div class="text-subtitle1">Famous City</div>
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="third" img-src="https://www.hindipanda.com/wp-content/uploads/2021/09/ecommerce-min.png">
+        <div class="absolute-bottom custom-caption">
+          <div class="text-h2">Third stop</div>
+          <div class="text-subtitle1">Famous Bridge</div>
+        </div>
+      </q-carousel-slide>
+    </q-carousel>
+  </div>
+</template>
+<style lang="sass" scoped>
+.custom-caption
+  text-align: center
+  padding: 12px
+  color: white
+  background-color: rgba(0, 0, 0, .3)
+</style>
