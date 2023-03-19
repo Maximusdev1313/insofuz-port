cb2d1af817090becce61886b817e3a61a24c6450
maximusdev1313 <maximusdev1313@gmail.com>
2023-03-19 21:17:36 +0500

    formatting code

--- src/pages/ProductsPage.vue
+++ src/pages/ProductsPage.vue
@@ -1,32 +1,30 @@
-
-
 <script setup>
-import {useRoute} from 'vue-router'
-import {ref, onMounted,watch} from 'vue'
-import {useApiStore} from 'src/stores'
-import card from 'src/components/card.vue'
-let store = useApiStore()
-let route = useRoute()
-onMounted(()=>{
-    store.getProducts(route.params.id)
-})
+import { useRoute } from "vue-router";
+import { ref, onMounted, watch } from "vue";
+import { useApiStore } from "src/stores";
+import card from "src/components/card.vue";
+let store = useApiStore();
+let route = useRoute();
+onMounted(() => {
+  store.getProducts(route.params.id);
+});
 
-    watch(
+watch(
   () => route.params.id,
-  (route) => (route = route <= store.categories.length  ? store.getProducts(route): route),
+  (route) =>
+    (route =
+      route <= store.categories.length ? store.getProducts(route) : route)
 );
 </script>
 
 <template>
-    <q-page>
-        <div>
-        detail 
-        {{ route.params.id }}
-<Suspense >
-    <card :products="store.products" v-if="store.products.length"/>
-
-</Suspense>
+  <q-page>
+    <div>
+      detail
+      {{ route.params.id }}
+      <Suspense>
+        <card :products="store.products" v-if="store.products.length" />
+      </Suspense>
     </div>
-    </q-page>
-   
+  </q-page>
 </template>
