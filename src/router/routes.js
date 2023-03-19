313b10c594ee8f805e0d36865a87fa57622ef889
maximusdev1313 <maximusdev1313@gmail.com>
2023-03-19 21:18:59 +0500

    add new route for packer, (detail: not ready) page

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,8 +6,12 @@ export const routes = [
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
       { path: 'order', component: () => import('pages/Order.vue') },
+      { path: 'packer', component: () => import('pages/Packer.vue') },
+      { path: 'detail', component: () => import('pages/ProductDetail.vue') },
       { path: '/user/:id/', name: 'user', component: () => import('pages/WaitRoom.vue') },
       { path: '/categoriya/:id/', component: () => import('pages/ProductsPage.vue') },
+
+
 
     ]
   },
