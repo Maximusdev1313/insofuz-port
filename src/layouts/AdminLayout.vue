113123fa445c7229a5399112432bfcf6156547f9
maximusdev1313 <maximusdev1313@gmail.com>
2023-03-31 21:43:56 +0500

    create: admin layout

---
+++ src/layouts/AdminLayout.vue
@@ -0,0 +1,52 @@
+<template>
+    <q-layout view="lHh Lpr lFf">
+      <q-header elevated dark>
+        <q-toolbar>
+          <q-btn
+            flat
+            dense
+            round
+            icon="menu"
+            aria-label="Menu"
+            @click="toggleLeftDrawer"
+          />
+          <router-link to="/" class="link">
+            <q-toolbar-title class="text-white" >
+            Quasar App
+          </q-toolbar-title>
+          </router-link>
+          
+          <q-space />
+         admin
+        </q-toolbar>
+      </q-header>
+  
+      <q-drawer
+        v-model="leftDrawerOpen"
+        bordered
+      >
+        <drawerContent/>
+      </q-drawer>
+  
+      <q-page-container>
+        <router-view />
+      </q-page-container>
+    </q-layout>
+  </template>
+  
+  <script setup>
+  import { defineComponent, ref } from 'vue'
+  import {useApiStore} from 'src/stores'
+
+  let leftDrawerOpen = ref(false)
+  function toggleLeftDrawer () {
+          leftDrawerOpen.value = !leftDrawerOpen.value
+        }
+  </script>
+  
+  <style scoped>
+  .link{
+    text-decoration: none;
+  }
+
+  </style>
