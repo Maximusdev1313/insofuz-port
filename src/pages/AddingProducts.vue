80b880a1b8ffee4378225e879290104791a702aa
maximusdev1313 <maximusdev1313@gmail.com>
2023-03-31 21:44:35 +0500

    create: admin page

---
+++ src/pages/AddingProducts.vue
@@ -0,0 +1,8 @@
+<script>
+
+</script>
+<template>
+    <div>
+        asdkjal
+    </div>
+</template>
---
+++ src/pages/Admin.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+import { useApiStore } from "src/stores";
+import categoryList from 'src/components/ForAdminPage/categoryList.vue'
+import inputForCategory from 'src/components/ForAdminPage/inputForCategory.vue'
+import categoryListVue from "src/components/ForAdminPage/categoryList.vue";
+const store = useApiStore()
+const submitForm = () => {
+};
+store.getCategory()
+</script>
+
+<template>
+    <div class="text-weight-bolder text-center text-h6 text-grey q-my-md">Kirish</div>
+
+  <div class="flex  row justify-around ">
+    <input-for-category/>
+    <div >
+      <div class="title">
+        Kategoriyalar
+      </div>
+      <category-list/>
+    </div>
+  </div>
+</template>
