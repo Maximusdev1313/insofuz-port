79ceda92e789b2b509d23fa093dfbd83b9295c06
maximusdev1313 <maximusdev1313@gmail.com>
2023-04-05 16:35:50 +0500

    change: setup productId and add cards

--- src/pages/AddingProducts.vue
+++ src/pages/AddingProducts.vue
@@ -3,6 +3,9 @@ import { ref } from "vue";
 import { useRoute } from "vue-router";
 import axios from "axios";
 import { useApiStore } from "src/stores";
+import card from 'src/components/card.vue';
+import alertDialog from "src/components/alertDialog.vue"; 
+
 const store = useApiStore()
 const route = useRoute();
 let options = ["Erkaklar", "Ayollar"];
@@ -14,8 +17,10 @@ let oldPrice = ref("");
 let imageLink = ref("");
 let isImportant = ref(false);
 let discountPersent = ref("");
-console.log(store.generateSpecialId()
-);
+let imgBtn = ref(false)
+let productId = ref('')
+
+store.getProducts(route.params.id)
 const getDiscountPersent = () => {
   if (oldPrice.value) {
     let persent = oldPrice.value / 100;
@@ -29,9 +34,11 @@ let urlForProducts = "http://insofuzlast.pythonanywhere.com/product/";
 let urlForImages = "http://insofuzlast.pythonanywhere.com/product-images/";
 const postProducts = async () => {
   getDiscountPersent();
+  store.setupId(productId, 'productId')
+  console.log(productId.value);
   try {
     const response = await axios.post(urlForProducts, {
-      id: store.generateSpecialId(),
+      id: productId.value,
       product: route.params.id,
       name: productName.value,
       discription: discription.value,
@@ -42,28 +49,46 @@ const postProducts = async () => {
       is_important: isImportant.value,
     });
     console.log(response.data);
+    imgBtn.value = true
     postImages()
+    store.getProducts(route.params.id)
+    emptyInputs()
+    store.isDone()
   } catch (error) {
     console.error(error.message);
+    store.alert = true
   }
 };
 const postImages = async () => {
-  console.log(store.specialId);
   try {
     const response = await axios.post(urlForImages, {
-      images: store.specialId,
+      images: productId.value,
       title: productName.value,
       image_link: imageLink.value
     });
     console.log(response.data);
   } catch (error) {
     console.error(error.message);
+    store.alert = true
+
   }
 };
+const emptyInputs = ()=>{
+  discription.value = ''
+  gender.value = ''
+  price.value = ''
+  oldPrice.value = ''
+  imageLink.value = ''
+  isImportant.value = false
+  discountPersent.value = ''
+}
+
 </script> 
 <template>
-  <div class="title text-center">Mahsulotlarni Kiritish</div>
-  <div class="row jsutify-between">
+  <alert-dialog/>
+  <q-btn @click="store.alert = true">bos</q-btn>
+  <div class="title text-center">Mahsulotlarni Kiritish: {{ store.category.category_name }}</div>
+  <div class="column">
     <q-form @submit.prevent="postProducts" class="forms q-gutter-md q-ma-md">
       <q-input v-model="productName" filled label="Mahsulot nomi" />
       <q-input
@@ -84,10 +109,21 @@ const postImages = async () => {
       <q-input v-model="imageLink" filled label="Rasm uchun link" />
       <q-checkbox v-model="isImportant" label="Muhim maxsulot" />
 
-      <div class="text-center">
-        <q-btn type="submit" label="Kirish" color="primary" />
+      <div class="row justify-between">
+        <div>
+          <q-btn @click="postImages" label="Rasm qo'shish" icon-right="library_add" class="q-mr-sm" v-if="imgBtn"/>
+
+        </div>
+
+        <q-btn type="submit" label="Mahsulot qo'shish" :icon="store.done ? 'done' : '' " color="primary" >
+          <!-- <q-icon name="done" ></q-icon> -->
+        </q-btn>
+
       </div>
+      
     </q-form>
-    <div class="products"></div>
+    <div class="products">
+      <card :products="store.products"/>
+    </div>
   </div>
 </template>
