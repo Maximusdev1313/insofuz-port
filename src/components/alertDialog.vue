a0e7ac21b8016252a1371b0d2d6daa6b90054a4b
maximusdev1313 <maximusdev1313@gmail.com>
2023-04-05 16:29:21 +0500

    create: alert dialog for errors

---
+++ src/components/alertDialog.vue
@@ -0,0 +1,27 @@
+<script setup>
+import {ref} from 'vue'
+import {useApiStore} from 'src/stores'
+const store = useApiStore()
+const reloadPage = ()=>{
+    location.reload()
+}
+</script>
+<template>
+     <q-dialog v-model="store.alert">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6 text-alert">Xatolik yuz berdi!</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+            Qandaydur xatolik yuz berdi. Iltimos internet aloqani tekshirib qaytadan urunib ko'ring!!!
+            Agarda xatolik bartaraf bo'lmasa mutahhasisga murojaat qiling. <br>
+            <div class="text-red text-center">Sahifa qayta yangilanadi!!!</div>
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="OK" color="primary" v-close-popup  @click="reloadPage"/>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+</template>
