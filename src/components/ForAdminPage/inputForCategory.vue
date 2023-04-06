29ac105bfec76f62617d65dda14579f93ec734fe
maximusdev1313 <maximusdev1313@gmail.com>
2023-04-06 14:58:32 +0500

    add: input rules

--- src/components/ForAdminPage/inputForCategory.vue
+++ src/components/ForAdminPage/inputForCategory.vue
@@ -8,6 +8,8 @@ let categoryName = ref("");
 let categoryImg = ref("");
 let idForCategory = ref('')
 
+const categoryRef = ref(null)
+const imgRef = ref(null)
 let urlForCategory = "http://insofuzlast.pythonanywhere.com/category/";
 let urlForImages = "http://insofuzlast.pythonanywhere.com/category-images/";
 
@@ -37,7 +39,6 @@ const postCategoryImages = async () => {
     console.error(error.message);
   }
 };
-
 const postAllData = () => {
   postCategoryName();
   postCategoryImages();
@@ -49,8 +50,8 @@ const postAllData = () => {
   <div class="forms">
     <div class="title">Kategoriyani kiriting</div>
     <q-form @submit.prevent="postAllData" class="q-gutter-md q-mt-md">
-      <q-input v-model="categoryName" filled hint="Kategoriya nomi" />
-      <q-input v-model="categoryImg" filled hint="Rasm uchun link" />
+      <q-input v-model="categoryName" filled hint="Kategoriya nomi" ref="categoryRef" :rules="[val => !!val || 'Majburiy maydon']"/>
+      <q-input v-model="categoryImg" filled hint="Rasm uchun link" ref="imgRef" :rules="[val => !!val || 'Majburiy maydon']"/>
 
       <div class="text-center">
         <q-btn type="submit" label="Kirish" color="primary" />
