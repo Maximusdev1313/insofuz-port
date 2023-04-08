ac00796d301e2c5ff963e6115eb058fdce9e7529
maximusdev1313 <maximusdev1313@gmail.com>
2023-04-09 00:34:57 +0500

    create: slider for main page

---
+++ src/components/slide.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { Splide, SplideSlide } from '@splidejs/vue-splide';
+import '@splidejs/vue-splide/css';
+import card from './card.vue';
+import {toRefs} from 'vue'
+import { useApiStore } from 'src/stores';
+const store = useApiStore()
+const props = defineProps({
+  products: Array,
+ 
+});
+const { products } = toRefs(props);
+const splideOptions = {
+        type: 'loop',
+        perPage: 3,
+        focus: 'center',
+        autoplay: true,
+        breakpoints: {
+          640: {
+            perPage: 2,
+          },
+          480: {
+            perPage: 1,
+          },
+        },
+      }
+    
+</script>
+<template>
+    <Splide :options="splideOptions" aria-label="My Favorite Images">
+      <SplideSlide v-for="product in products" :key="product">
+        <q-img :src="product.images[0]?.image_link " :alt="product.name" class="q-pa-md">
+            <div class="absolute-full column ">
+                <div class="discount row justify-between">
+                    <div class="discount__title">
+                        Chegirma
+                    </div>
+                    <div class="discount__percent">
+                    {{ product.discount }} %
+
+                    </div>
+                </div>
+                <div class="product-title row justify-center items-center text-h5 text-uppercase">
+                    {{ product.name }}
+                </div>
+
+            </div>
+        </q-img>
+    </SplideSlide>
+    </Splide>
+  </template>
+<style scoped>
+.product-title{
+    height: 80%;
+}
+
+</style>
