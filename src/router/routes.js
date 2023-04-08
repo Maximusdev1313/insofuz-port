1cc89af9f22715f6d7b953dcef339c612d272b55
Alimardon9459 <mullajonovalimardon@gmail.com>
2023-04-08 23:44:22 +0500

    change packer page link

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,8 +5,7 @@ export const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'order', component: () => import('pages/Order.vue') },
-      { path: 'packer', component: () => import('pages/Packer.vue') },
+      { path: '/order', component: () => import('pages/Order.vue') },
       { path: '/product/:id', component: () => import('pages/ProductDetail.vue') },
       { path: '/user/:id/', name: 'user', component: () => import('pages/WaitRoom.vue') },
       { path: '/category/:id/', component: () => import('pages/ProductsPage.vue') },
@@ -19,10 +18,11 @@ export const routes = [
     path: '/admin',
     component: () => import('layouts/AdminLayout.vue'),
     children: [
-      
+
       { path: '/admin/', component: () => import('pages/Admin.vue') },
       { path: '/admin/category/:id', component: () => import('pages/AddingProducts.vue') },
-     
+      { path: '/admin/packer', component: () => import('pages/Packer.vue') },
+
 
 
     ]
