1c061b1fbde3a7e1229497ee2c0c962fa7cfc067
maximusdev1313 <maximusdev1313@gmail.com>
2023-04-10 17:32:11 +0500

    add: path for report page

--- src/router/routes.js
+++ src/router/routes.js
@@ -22,6 +22,7 @@ export const routes = [
       
       { path: '/admin/', component: () => import('pages/Admin.vue') },
       { path: '/admin/category/:id', component: () => import('pages/AddingProducts.vue') },
+      { path: '/admin/report', component: () => import('pages/ReportPage.vue') },
      
 
 
