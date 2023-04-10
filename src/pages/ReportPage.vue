7f0aca8fa9033a8b56767509ab422b0dbdb389ff
maximusdev1313 <maximusdev1313@gmail.com>
2023-04-10 17:31:49 +0500

    create: report page

---
+++ src/pages/ReportPage.vue
@@ -0,0 +1,28 @@
+<script setup>
+import axios from 'axios'
+import {ref} from 'vue'
+import { useRoute } from 'vue-router'
+
+const ordersLength = ref()
+const users = ref()
+const getProducts = async () =>{
+    const Response = await axios.get('http://insofuzlast.pythonanywhere.com/product/')
+    console.log(Response.data);
+}
+const getOrders = async () =>{
+    const Response = await axios.get('http://insofuzlast.pythonanywhere.com/user/')
+    const Api = Response.data
+    ordersLength.value = Api.length
+    users.value = Api
+    console.log(users.value);
+    
+}
+getProducts()
+getOrders()
+</script>
+
+<template>
+    <q-page>
+        
+    </q-page>
+</template>
