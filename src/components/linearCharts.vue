7fbcfc254675cdfa43267487affea509d12a57b1
maximusdev1313 <maximusdev1313@gmail.com>
2023-04-13 19:42:04 +0500

    create: charts

---
+++ src/components/linearCharts.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+<template>
+    <div>
+        <table class="charts-css line show-heading show-labels show-data-axes show-primary-axis">
+
+        </table>
+    </div>
+</template>
