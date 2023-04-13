678b77b413f30fbb0e6f94865579347cf6a5500d
maximusdev1313 <maximusdev1313@gmail.com>
2023-04-13 19:41:04 +0500

    create: dialog for reports

---
+++ src/components/dialogForReports.vue
@@ -0,0 +1,36 @@
+<script setup>
+import {ref} from 'vue'
+
+const dialog = ref(false)
+    const position = ref('top')
+
+ const   open = (pos) => {
+        position.value = pos
+        dialog.value = true
+      }
+</script>
+
+<template>
+    <div>
+        <q-btn label="Top" icon="keyboard_arrow_up" color="primary" @click="open('top')" />
+    
+        <q-dialog v-model="dialog" :position="position">
+      <q-card style="width: 350px">
+        <q-linear-progress :value="0.6" color="pink" />
+
+        <q-card-section class="row items-center no-wrap">
+          <div>
+            <div class="text-weight-bold">The Walker</div>
+            <div class="text-grey">Fitz & The Tantrums</div>
+          </div>
+
+          <q-space />
+
+          <q-btn flat round icon="fast_rewind" />
+          <q-btn flat round icon="pause" />
+          <q-btn flat round icon="fast_forward" />
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+    </div>
+</template>
